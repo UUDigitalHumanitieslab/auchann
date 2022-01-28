683292673e9018f1505f3982de363dbd1ccad6f8
Sheean Spoel <sheean@sheean.nl>
2022-01-28 15:37:55 +0100

    requirements.txt

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='0.1.0',
     packages=find_packages(include=['auchann', 'auchann.*']),
     install_requires=[
-        'chamd'
+        'chamd',
+        'editdistance',
+        'numpy'
         ]
 )
