a3e29a9c3050a99fb12fa08d4dc5ad0d685231ff
Mees <meesstip@gmail.com>
2022-02-25 10:05:27 +0100

    update test

--- unit-tests/test_align.py
+++ unit-tests/test_align.py
@@ -3,14 +3,35 @@ from auchann.align_words import align_words2
 
 
 class TestAlign(unittest.TestCase):
-    def test_align(self):
+    def test_replace(self):
         transcript_line = "Sheean is dee man"
         correction_line = "Sheean is de man"
+        expected_chat_line = "Sheean is dee [: de] man"
 
         aligned_tokens = align_words2(transcript_line, correction_line)
         chat_line = ' '.join(object.__str__() for object in aligned_tokens)
         expected_chat_line = "Sheean is dee [: de] man"
         self.assertEqual(chat_line, expected_chat_line)
+
+    def test_remove(self):
+        transcript_line = "Sheean is dee beste man"
+        correction_line = "Sheean is de man"
+        expected_chat_line = "Sheean is dee [: de] <beste> [///] man"
+
+        aligned_tokens = align_words2(transcript_line, correction_line)
+        chat_line = ' '.join(object.__str__() for object in aligned_tokens)
+        self.assertEqual(chat_line, expected_chat_line)
+
+    def test_insert(self):
+        transcript_line = "Sheean is dee man"
+        correction_line = "Sheean is de beste man"
+        expected_chat_line = "Sheean is dee [: de] 0beste man"
+
+        aligned_tokens = align_words2(transcript_line, correction_line)
+        chat_line = ' '.join(object.__str__() for object in aligned_tokens)
+        self.assertEqual(chat_line, expected_chat_line)
+
+
 
 
 if __name__ == '__main__':
