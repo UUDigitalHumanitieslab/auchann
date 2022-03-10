4769a3ae83d2a79598d34377850088e0b95abb0b
Sheean Spoel <sheean@sheean.nl>
2022-03-10 15:26:27 +0100

    Leaky abstraction

--- unit-tests/test_align.py
+++ unit-tests/test_align.py
@@ -8,29 +8,33 @@ class TestAlign(unittest.TestCase):
         correction_line = "doet zij even de armen weg"
         expected_chat_line = "doet zij even de armen wes [: weg]"
 
-        alignment = align_words(transcript_line, correction_line)
-        chat_line = ' '.join(str(correction) for correction in alignment.corrections)
-        self.assertEqual(chat_line, expected_chat_line)
+        self.assertAlign(transcript_line, correction_line, expected_chat_line)
 
     def test_remove(self):
         transcript_line = "alleen dit dit"
         correction_line = "alleen dit"
         expected_chat_line = "alleen dit [/] dit"
 
-        alignment = align_words(transcript_line, correction_line)
-        chat_line = ' '.join(str(correction) for correction in alignment.corrections)
-        self.assertEqual(chat_line, expected_chat_line)
+        self.assertAlign(transcript_line, correction_line, expected_chat_line)
 
     def test_insert(self):
         transcript_line = "magge zien"
         correction_line = "mag ik zien"
         expected_chat_line = "magge [: mag] 0ik zien"
 
+        self.assertAlign(transcript_line, correction_line, expected_chat_line)
+
+    def test_repetition(self):
+        transcript_line = "toen kwam hij bij een een weiland"
+        correction_line = "toen kwam hij bij een weiland"
+        expected_chat_line = "toen kwam hij bij een [/] een weiland"
+
+        self.assertAlign(transcript_line, correction_line, expected_chat_line)
+
+    def assertAlign(self, transcript_line: str, correction_line: str, expected_chat_line: str):
         alignment = align_words(transcript_line, correction_line)
-        chat_line = ' '.join(str(correction) for correction in alignment.corrections)
+        chat_line = str(alignment)
         self.assertEqual(chat_line, expected_chat_line)
-
-
 
 
 if __name__ == '__main__':
